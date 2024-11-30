67b8947da2fa28f5924afa382986bbb73a047fd5
Sudarapradeep <Sudarapradeep0@gmail.com>
2024-11-30 22:53:29 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,9 +8,9 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "qvI1AAiB#FPIctHERRPwu-t59cjpYJAXA14r_jrneD47JoniP6gQ", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_VOICE: process.env.AUTO_VOICE || "flase",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-AUTO_REACT: process.env.AUTO_REACT || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",    
 };
